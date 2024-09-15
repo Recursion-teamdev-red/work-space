7de542b2bb9b15ccfc1e2d77ef87e5ca31c477b3
Tai <100829535+uiuxadeadev@users.noreply.github.com>
2024-09-15 23:59:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
 function drawOmikuji() {
     // S1: おみくじの結果をオブジェクトで格納（画像URLを含む）
-
+    const omikujiResults = [
+        {
+            fortune: '大吉 - Great blessing!',
+            advice: 'リファクタリングに成功してプログラムが爆速に!!サクサク動いて気持ちいいーーー!!',
+            language: 'PHP',
+            database: 'Oracle Database',
+            editor: 'nano',
+            image: 'https://example.com/daikichi.png' // 大吉の画像URL
+        },
+        {
+            fortune: '中吉 - Middle blessing!',
+            advice: 'リファクタリングに成功してプログラムが爆速に!!サクサク動いて気持ちいいーーー!!',
+            language: 'PHP',
+            database: 'Oracle Database',
+            editor: 'nano',
+            image: 'https://example.com/chukichi.png' // 中吉の画像URL
+        },
+        {
+            fortune: '小吉 - Small blessing!',
+            advice: 'リファクタリングに成功してプログラムが爆速に!!サクサク動いて気持ちいいーーー!!',
+            language: 'PHP',
+            database: 'Oracle Database',
+            editor: 'nano',
+            image: 'https://example.com/shokichi.png' // 小吉の画像URL
+        },
+        {
+            fortune: '吉 - Blessing',
+            advice: 'リファクタリングに成功してプログラムが爆速に!!サクサク動いて気持ちいいーーー!!',
+            language: 'PHP',
+            database: 'Oracle Database',
+            editor: 'nano',
+            image: 'https://example.com/kichi.png' // 吉の画像URL
+        },
+        {
+            fortune: '末吉 - Future blessing!',
+            advice: 'リファクタリングに成功してプログラムが爆速に!!サクサク動いて気持ちいいーーー!!',
+            language: 'PHP',
+            database: 'Oracle Database',
+            editor: 'nano',
+            image: 'https://example.com/suekichi.png' // 末吉の画像URL
+        },
+        {
+            fortune: '凶 - Bad luck...',
+            advice: 'リファクタリングに成功してプログラムが爆速に!!サクサク動いて気持ちいいーーー!!',
+            language: 'PHP',
+            database: 'Oracle Database',
+            editor: 'nano',
+            image: 'https://example.com/kyo.png' // 凶の画像URL
+        }
+    ];
     // S2: ランダムにインデックスを選択
     
     // S3: 案内画像を非表示にする
