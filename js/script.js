7b11828f8c052eb147d42ad2e901632ea023114a
Tai <taiu.engineer@gmail.com>
2024-09-16 01:38:48 +0900

    initial setup

--- js/script.js
+++ js/script.js
@@ -3,5 +3,7 @@ import { drawOmikuji } from './drawOmikuji.js';
 import { resetOmikuji } from './resetOmikuji.js';
 
 // drawOmikuji関数をボタンにバインド
+document.getElementById('draw-button').addEventListener('click', drawOmikuji);
 
 // resetOmikuji関数をボタンにバインド
+document.getElementById('reset-button').addEventListener('click', resetOmikuji);
