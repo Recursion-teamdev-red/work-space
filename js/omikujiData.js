2a1c8105c0d1bb464efa5c7698a1feeb664da5a3
Tai <taiu.engineer@gmail.com>
2024-09-19 01:40:19 +0900

    HTMLタグをindex.htmlに移行

--- js/omikujiData.js
+++ js/omikujiData.js
@@ -1,6 +1,6 @@
 export const omikujiResults = [
   {
-    fortune: '大吉 - Great blessing!',
+    fortune: '大吉 <br> Great blessing!',
     advice: 'GitHub Sponsors を通じて太っ腹なスポンサーがつきます。これであなたの OSS 開発は安泰です!!',
     language: 'Java',
     database: 'SQL Server',
@@ -8,7 +8,7 @@ export const omikujiResults = [
     image: '../assets/image_Great blessing.png', // 大吉の画像URL
   },
   {
-    fortune: '中吉 - Middle blessing!',
+    fortune: '中吉 <br> Middle blessing!',
     advice:
       'リリースしたソフトウェアに深刻なバグが見つかりますが、口八丁な営業のお陰で事無きを得るでしょう。ただし、後日それを理由に無茶振りされるおそれがあるので油断は禁物です。',
     language: 'TypeScript',
@@ -17,7 +17,7 @@ export const omikujiResults = [
     image: '../assets/image_Middle blessing!.png', // 中吉の画像URL
   },
   {
-    fortune: '小吉 - Small blessing!',
+    fortune: '小吉 <br> Small blessing!',
     advice: '開発の要件定義が綿飴のようにふわっふわです。手遅れにならないうちに手を打っておくのが吉。',
     language: 'C♯',
     database: 'Oracle Database',
@@ -25,7 +25,7 @@ export const omikujiResults = [
     image: '../assets/Image_Small blessing.png', // 小吉の画像URL
   },
   {
-    fortune: '吉 - Blessing',
+    fortune: '吉 <br> Blessing',
     advice:
       'リリースしたソフトウェアにバグが見つかりますが、幸いユーザー企業のお偉方は極度のITオンチです。仕様で押し通すのが吉。',
     language: 'Ruby',
@@ -34,7 +34,7 @@ export const omikujiResults = [
     image: '../assets/image_Blessing.png', // 吉の画像URL
   },
   {
-    fortune: '末吉 - Future blessing!',
+    fortune: '末吉 <br> Future blessing!',
     advice: 'リファクタリングに成功してプログラムが爆速に!!サクサク動いて気持ちいいーーー!!',
     language: 'Python',
     database: 'MySQL',
@@ -42,7 +42,7 @@ export const omikujiResults = [
     image: '../assets/image_Future blessing.png', // 末吉の画像URL
   },
   {
-    fortune: '凶 - Bad luck...',
+    fortune: '凶 <br> Bad luck...',
     advice:
       'リリースしたソフトウェアに致命的な欠陥が見つかり、それが原因で顧客の機密情報が派手に流出します。仕様で押し通すにはいささか無理がありますので、潔く腹をくくりましょう。',
     language: 'Python',
--- js/resetOmikuji.js
+++ js/resetOmikuji.js
@@ -1,20 +1,50 @@
 export function resetOmikuji() {
   // 結果表示エリアを非表示
   // 案内画像とおみくじボタンを再表示し、リセットボタンを非表示
+  const resultAreaEle = document.getElementById('result-container');
   const fortuneEle = document.getElementById('fortune');
   const adviceEle = document.getElementById('advice');
+  const languageEle = document.getElementById('language');
+  const databaseEle = document.getElementById('database');
+  const editorEle = document.getElementById('editor');
   const imageEle = document.getElementById('omikuji-image');
   const guideImage = document.getElementById('guide-image');
   const omikujiButton = document.getElementById('draw-button');
   const resetButton = document.getElementById('reset-button');
-  hideResult(fortuneEle,adviceEle,imageEle,guideImage,omikujiButton,resetButton);
+  hideResult(
+    resultAreaEle,
+    fortuneEle,
+    adviceEle,
+    languageEle,
+    databaseEle,
+    editorEle,
+    imageEle,
+    guideImage,
+    omikujiButton,
+    resetButton
+  );
 }
 
-function hideResult(fortuneEle,adviceEle,imageEle,guideImage,omikujiButton,resetButton) {
-  fortuneEle.style.display = "none";
-  adviceEle.style.display = "none";
-  imageEle.style.display = "none";
-  guideImage.style.display = "block";
-  omikujiButton.style.display = "inline-block";
-  resetButton.style.display = "none";
+function hideResult(
+  resultAreaEle,
+  fortuneEle,
+  adviceEle,
+  languageEle,
+  databaseEle,
+  editorEle,
+  imageEle,
+  guideImage,
+  omikujiButton,
+  resetButton
+) {
+  resultAreaEle.style.display = 'none';
+  fortuneEle.style.display = 'none';
+  adviceEle.style.display = 'none';
+  languageEle.style.display = 'none';
+  databaseEle.style.display = 'none';
+  editorEle.style.display = 'none';
+  imageEle.style.display = 'none';
+  guideImage.style.display = 'block';
+  omikujiButton.style.display = 'inline-block';
+  resetButton.style.display = 'none';
 }
